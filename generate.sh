debc479ccd9b907fa6dca5e2b81e5a0df78d94cb
seykron <seykron@riseup.net>
2023-02-05 18:01:31 -0300

    Adds gitlab account link to template

--- generate.sh
+++ generate.sh
@@ -6,12 +6,12 @@ OUTPUT_DIR=./docs
 pandoc --template $SOURCE_DIR/template.html -o $OUTPUT_DIR/cv.es.html $SOURCE_DIR/cv.es.markdown
 pandoc --template $SOURCE_DIR/template.odt -t odt -o $OUTPUT_DIR/cv.es.odt $SOURCE_DIR/cv.es.markdown
 pandoc -o $OUTPUT_DIR/cv.es.txt $SOURCE_DIR/cv.es.markdown
-wkhtmltopdf $OUTPUT_DIR/cv.es.html $OUTPUT_DIR/cv.es.pdf
+wkhtmltopdf --enable-local-file-access $OUTPUT_DIR/cv.es.html $OUTPUT_DIR/cv.es.pdf
 
 pandoc --template $SOURCE_DIR/template.html -o $OUTPUT_DIR/cv.en.html $SOURCE_DIR/cv.en.markdown
 pandoc --template $SOURCE_DIR/template.odt -t odt -o $OUTPUT_DIR/cv.en.odt $SOURCE_DIR/cv.en.markdown
 pandoc -o $OUTPUT_DIR/cv.en.txt $SOURCE_DIR/cv.en.markdown
-wkhtmltopdf $OUTPUT_DIR/cv.en.html $OUTPUT_DIR/cv.en.pdf
+wkhtmltopdf --enable-local-file-access $OUTPUT_DIR/cv.en.html $OUTPUT_DIR/cv.en.pdf
 
 # Creates indexes
 cp $OUTPUT_DIR/cv.en.html $OUTPUT_DIR/index.html
